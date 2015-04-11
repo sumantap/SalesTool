77cb41c1305720d1eb810a088fdf81c6ebc46472
sumanta <sumantap19@gmail.com>
2015-04-12 01:59:36 +0530

    Initial Checkin of sales tool.

---
+++ GitTest/src/com/test/sales/Main.java
@@ -0,0 +1,15 @@
+package com.test.sales;
+
+public class Main {
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		displayGreetings();
+		data.display();
+	}
+
+	private static void displayGreetings() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Happy Sales People");
+		System.out.println("This shows sales data");
+	}
+}
---
+++ GitTest/src/com/test/sales/SalesData.java
@@ -0,0 +1,8 @@
+package com.test.sales;
+
+public class SalesData {
+	int data[] = {0,44,12};
+	public void display(){
+		System.out.println("TODO: Implement data display");
+	}
+}
