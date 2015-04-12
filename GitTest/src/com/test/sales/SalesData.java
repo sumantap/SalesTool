859057a287816b44005eeb373dbe8442e2de3b3d
kuchu <sumantap.pattanayak@gmail.com>
2015-04-12 07:52:18 +0530

    Sum values are added

--- GitTest/src/com/test/sales/SalesData.java
+++ GitTest/src/com/test/sales/SalesData.java
@@ -3,6 +3,11 @@ package com.test.sales;
 public class SalesData {
 	int data[] = {0,44,12};
 	public void display(){
-		System.out.println("TODO: Implement data display");
+		int sum = 0;
+		for(int i=0;i<data.length;i++) {
+			sum += i;
+			System.out.println("TODO: Implement data display" + data[i]);
+		}
+		System.out.println("sum value are:" + sum);
 	}
 }
